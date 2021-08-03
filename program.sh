4ba93a45aa33cdd72a7c25dc35fe4a45f50add51
galactica <galactica@libraries>
2021-08-03 20:29:39 +0300

    we revert "i use deps-repl library"
    This reverts commit 299f01679a1f15d1f9af5b61bb10130b19b34d2c.

--- program.sh
+++ program.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-repl(){
-  clj \
-    -X:repl deps-repl.core/process \
-    :main-ns galactica.main \
-    :port 7788 \
-    :host '"0.0.0.0"' \
-    :repl? true \
-    :nrepl? false
-}
 
 "$@"
