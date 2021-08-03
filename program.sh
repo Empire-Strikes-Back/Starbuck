23af663708705cc0c9ec00c81ae557d5ecd1fb76
galactica <galactica@program>
2021-08-03 18:50:37 +0300

    to program.sh i add j-package

--- program.sh
+++ program.sh
@@ -28,6 +28,31 @@ uberjar(){
   mv out/galactica.standalone.jar out/jpackage-input/
 }
 
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting compilation..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut"
+  else
+      J_ARG=""
+  fi
+
+  jpackage \
+    --input out/jpackage-input \
+    --dest out \
+    --main-jar galactica.standalone.jar \
+    --name "galactica" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments galactica.main \
+    --app-version "1" \
+    $J_ARG
+}
 
 
 "$@"
